92ee63f58165d88860f79a8dba9019b01a1517bf
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-10-30 23:08:12 +0530

    context api project

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -15,17 +15,19 @@ function Login() {
 
   return (
      <>
-        <h2>Login</h2>
-        <div className='flex flex-col w-1/3 gap-5 m-auto mt-5'>
+        <h2 className='text-center text-3xl text-green-900 font-bold'>Login</h2>
+        <div className='flex flex-col w-1/3 gap-5 m-auto mt-5 bg-slate-400 p-5'>
             <p>User Name</p>
             <input type="text" placeholder='username' 
             value={username}
-            onChange={(e)=>setUsername(e.target.value)}/>
+            onChange={(e)=>setUsername(e.target.value)}
+            className='px-2 rounded-md'/>
             {" "}
             <p>Password</p>
             <input type="text" placeholder='password' 
             value={password}
-            onChange={(e)=>setPassword(e.target.value)}/>
+            onChange={(e)=>setPassword(e.target.value)}
+            className='px-2 rounded-md'/>
             <br />
             <button onClick={handleSubmit} className='bg-blue-600 px-5 py-1 rounded-lg text-white font-semibold'>Submit</button>
         </div>
--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -4,9 +4,9 @@ import UserContext from '../context/UserContext'
 function Profile() {
     const {user} = useContext(UserContext)
 
-    if (!user) return <h2>Please Login</h2>
+    if (!user) return <h2 className='text-center text-2xl mt-3 bg-black px-3 py-1 rounded-md w-fit m-auto text-white'>Please Login</h2>
 
-    return <div>Welcome {user.username}</div>
+    return <div className='text-center text-2xl mt-3 bg-black px-3 py-1 rounded-md w-fit m-auto text-white'>Welcome {user.username}</div>
 }
 
 export default Profile
